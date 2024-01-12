0e63b4620ec36b90291433d62c4b40981aaf6f3d
Ashish Khanal <affableashk@outlook.com>
2024-01-12 00:03:27 -0500

    Clickable boxes with "X"

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,3 +1,39 @@
-export default function Square() {
-  return <button className="square">XYZ</button>;
+import { useState } from "react";
+
+function Square() {
+  console.log("square box created!");
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+    console.log("square box clicked!");
+  }
+
+  return (
+      <button className="square" onClick={handleClick}>
+        {value}
+      </button>
+  );
+}
+
+export default function Board() {
+  return (
+      <>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+      </>
+  );
 }
