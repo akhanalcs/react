c3c79eff91ca2a18191a487f70b0c4d1c6a456da
Ashish Khanal <affableashk@outlook.com>
2024-01-16 16:28:51 -0500

    Working Redux!

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,12 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { add, reset } from "./features/counter/counterSlice";
+
 export default function App() {
   console.log("Hey App is rendered!");
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  console.log(add);
+
   return (
-      <div>
-        <h1>Welcome to my counter</h1>
-        <p>Count: 1</p>
-        <button onClick={() => 1}>Add</button>
-        <button onClick={() => 1}>Add 5</button>
-        <button onClick={() => 1}>Reset</button>
-      </div>
+    <div>
+      <h1>Welcome to my counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(add(1))}>Add</button>
+      <button onClick={() => dispatch(add(5))}>Add 5</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
   );
 }
--- redux-example/src/features/counter/counterSlice.js
+++ redux-example/src/features/counter/counterSlice.js
@@ -1,7 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    value: 0
-}
+  value: 0,
+};
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  // reducers object
+  reducers: {
+    // Redux Toolkit allows us to write "mutating" logic in reducers. It
+    // doesn't actually mutate the state because it uses the Immer library,
+    // which detects changes to a "draft state" and produces a brand new
+    // immutable state based off those changes
+    add: (state, action) => {
+      state.value += action.payload;
+      //console.log(action);
+    },
+    reset: () => initialState,
+    // Works same as above
+    // reset: (state) => {
+    //   state.value = initialState.value;
+    // },
+  },
+});
 
-
+// Action creators are generated for each reducer function
+// They're called like this from the UI: {() => dispatch(add(1))}
+// The action "type" are named: "counter/add" and "counter/reset"
+// The "payload" is 1 (for above example of dispatch(add(1)))
+export const { add, reset } = counterSlice.actions;
+export default counterSlice.reducer;
